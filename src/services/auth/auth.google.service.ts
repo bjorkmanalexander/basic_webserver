dabc804150928ca9d03b3ad6aeae363776a33efb
absoluteRango <31278752+absoluteRango@users.noreply.github.com>
2021-01-01 19:58:12 +0100

    Removed locals in auth

--- src/services/auth/auth.google.service.ts
+++ src/services/auth/auth.google.service.ts
@@ -24,6 +24,5 @@ passport.use(new Strategy({
 
 router.get("/google", passport.authenticate('google', { scope: [ 'profile', 'email' ] } ));
 router.get("/google/redirect", passport.authenticate('google', { failureRedirect: "/"} ), async (req: Request, res: Response) => {
-    res.locals.user = req.user;
     res.redirect("/success");
 });
